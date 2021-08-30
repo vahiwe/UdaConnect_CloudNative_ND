042e5ba6592c79228b4f31455081205e9a0d772f
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2021-08-30 23:36:59 +0100

    Update Kubernetes deployment files

--- modules/api-person-service/app/udaconnect/controllers.py
+++ modules/api-person-service/app/udaconnect/controllers.py
@@ -13,7 +13,7 @@ from typing import Optional, List
 
 DATE_FORMAT = "%Y-%m-%d"
 
-api = Namespace("UdaConnect", description="Connections via geolocation.")  # noqa
+api = Namespace("UdaConnect Person Service", description="Connections via geolocation.")  # noqa
 
 
 # TODO: This needs better exception handling
